09e37be51116edea81e073903040c70d32d77d70
grodier <grodier3@gmail.com>
2022-11-15 21:00:13 -0500

    Add styles to footer

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./styles/tailwind.config.js");
   eleventyConfig.addWatchTarget("./styles/tailwind.css");
 
+  eleventyConfig.addPassthroughCopy("img");
+
   eleventyConfig.addShortcode("version", function () {
     return now;
   });
